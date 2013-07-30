bb00098aef62f5b15ddb4655dd6a9b3a5b7c2438
Gonzalo Bellver <gonzalo.bellver@gmail.com>
2013-07-30 13:37:50 -0300

    Step 2 draft

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -19,7 +19,7 @@ controllers.controller('ResourceDetailController', ['$scope', '$rootScope', func
 
     $scope.steps = [
         { step: 0, title: "Indentificación de la organización", template:"partials/form/step1.html", completed: false },
-        { step: 1, title: "Dirección", template:"partials/form/step2.html", completed: false },
+        { step: 1, title: "Dirección", template:"partials/form/step2.html", completed: false, onload: "$scope.initMap()" },
         { step: 2, title: "Tipo de organización", template:"partials/form/step3.html", completed: false },
         { step: 3, title: "Actividades de la organización", template:"partials/form/step4.html", completed: false },
         { step: 4, title: "Información adicional", template:"partials/form/step5.html", completed: false }
@@ -33,8 +33,10 @@ controllers.controller('ResourceDetailController', ['$scope', '$rootScope', func
             $scope.steps[$scope.currentStep].completed = true;
             $scope.completed++;
         }
-
         $scope.currentStep++;
+        if ($scope.steps[$scope.currentStep].onload) {
+            eval($scope.steps[$scope.currentStep].onload);
+        }
 
     }
 
@@ -51,5 +53,17 @@ controllers.controller('ResourceDetailController', ['$scope', '$rootScope', func
         $(".progress-bar").css("width", "100%");
         $scope.steps[$scope.currentStep].completed = true;
         console.log("submit the form");        //TODO(gb): Remove trace!!!
+    }
+
+    $scope.initMap = function() {
+        if (!$scope.mapLoaded) {
+            var mapOptions = {
+                center: new google.maps.LatLng(-34.63123, -58.441772),
+                zoom: 11,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            var map = new google.maps.Map(document.getElementById("address-map"), mapOptions);
+        }
+        $scope.mapLoaded = true;
     }
 }])
